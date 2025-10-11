b289383b699e3085921bbb342a6784c117fa7d62
Akhilesh Pardeshi <akhileshpardeshi3@gmail.com>
2025-10-11 04:35:15 +0000

    11-10-2025

--- try.c
+++ try.c
@@ -1,32 +1,85 @@
 #include<stdio.h>
-int main(){
-    int sum_wait=0,sum_turnaround=0,at[10],bt[10],priority[10],remain,smallest,n,time;
-    printf("Enter the p0");
-    scanf("%d",&n);    
-    remain=n;
-    for (int i = 0; i < n; i++)
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+void typeline(char *c,char *filename)
+{
+    int handle,i=1;
+    char ch;
+    handle = open(filename,O_RDONLY);
+    if(handle == -1)
     {
-        printf("Enter the at,bt,priority of %d process",i+1);
-        scanf("%d%d%d",&at[i],&bt[i],&priority[i]);
-        /* code */
+        printf("File NOt FOUND");
+        return;
     }
-    priority[9]=11;
-    for(time=0;remain=0;)
+
+    if(strcmp(c,"a")==0)
     {
-        smallest=0;
-        for (int i = 0; i < n; i++)
+        while(read(handle,&ch,1)!=0)
         {
-            if(at[i]<time&&priority[i]<priority[smallest]&&bt[i]>0){
-                smallest=i;
-            }
-            /* code */
+            printf("%c",ch);
         }
-        time+=bt[smallest];
-        remain--;
-        sum_wait+=time-at[smallest]-bt[smallest];
-        sum_turnaround+=time-at[smallest];
-        bt[smallest]=0;
+        close(handle);
+        return;
     }
-    printf("%d%d",sum_wait,sum_turnaround);
+
+    int n = atoi(c);
     
+    if(n>0)
+    {
+        while(read(handle,&ch,1) != 0)
+        {
+            if(ch=='\n')
+            i++;
+            if(i==n)
+            break;
+            printf("%c",ch);
+        }
+    }
+}
+
+int main()
+{
+    char command[50],t1[10],t2[10],t3[10],t4[10];
+    int n;
+    system("clear");
+    while(1)
+    {
+        printf("$myshell $");
+        fflush(stdin);
+        fgets(command,50,stdin);
+        n = sscanf(command,"%s %s %s %s", t1, t2, t3, t4);
+        switch(n)
+        {
+            case 1:
+                if(strcmp(t1,"exit")==0)
+                exit(0);
+                if(!fork())
+                {
+                    execlp(t1,t1,NULL);
+                }
+                break;
+            case 2:
+                if(!fork())
+                {
+                    execlp(t1,t2,NULL);
+                }
+                break;
+            case 3:
+                if(strcmp(t1,"typeline")==0)
+                {
+                    typeline(t2,t3);
+                }
+                if(!fork())
+                {
+                    execlp(t1,t1,t2,t3,NULL);
+                    perror(t1);
+                }
+                break;
+        }
+    }
 }
